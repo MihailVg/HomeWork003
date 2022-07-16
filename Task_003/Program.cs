f8ff8d167ee4397476e289648625e1c49d558376
VinogradovMihail <misha-vinogradov-1997@mail.ru>
2022-07-16 14:16:00 +0300

    Task003 done

---
+++ Task_003/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача N3, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число, кубы до которого будут высчитываться: ");
+
+int N = int.Parse(Console.ReadLine());
+int numberA = 1;
+
+if (N < 0)
+{
+    while (numberA > N)
+    {
+        Console.Write(numberA * numberA * numberA + ", ");
+        numberA = numberA -1;
+    }
+    Console.Write(numberA * numberA * numberA);
+}
+
+if (N > 0)
+{
+    while (numberA < N)
+    {
+        Console.Write(numberA * numberA * numberA + ", ");
+        numberA++;
+    }
+    Console.Write(numberA * numberA * numberA);
+}
