ed63f2457b10653239be58fc56918a13e75eb5ad
VinogradovMihail <misha-vinogradov-1997@mail.ru>
2022-07-16 14:48:31 +0300

    Task002 done

---
+++ Task_002/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача N2, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+
+Console.Write("Введите координаты x, y и z, обозначающие первую точку: ");
+double x = double.Parse(Console.ReadLine());
+double y = double.Parse(Console.ReadLine());
+double z = double.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты x1, y1 и z1, обозначающие вторую точку: ");
+double x1 = double.Parse(Console.ReadLine());
+double y1 = double.Parse(Console.ReadLine());
+double z1 = double.Parse(Console.ReadLine());
+
+double numberA = x - x1;
+double numberB = y - y1;
+double numberC = z - z1;
+
+double Distance = Math.Sqrt(numberA * numberA + numberB * numberB + numberC * numberC);
+
+Console.WriteLine("Расстояние между точками равно: " + Distance);
